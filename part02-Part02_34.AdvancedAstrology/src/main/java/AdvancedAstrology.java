040fd67f63e874a95efca9d588c20d46da2b6503
OZsoft-zac <81825073+OZsoft-zac@users.noreply.github.com>
2021-08-30 11:50:59 -0500

    Add files via upload

---
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -0,0 +1,63 @@
+
+public class AdvancedAstrology {
+
+    public static void printStars(int number) {
+        // part 1 of the exercise
+        int i = 0;
+        while(i < number){
+            System.out.print("*");
+            i++;
+        }
+        System.out.println("");
+    }
+
+    public static void printSpaces(int number) {
+        // part 1 of the exercise
+        int i = 0;
+        while(i < number){
+            System.out.print(" ");
+            i++;
+        }
+    }
+
+    public static void printTriangle(int size) {
+        // part 2 of the exercise
+        int i = 1;
+        int j = size - 1;
+        while(i <= size && j >= 0){
+            printSpaces(j);
+            printStars(i);
+            i++;
+            j--;
+        }
+    }
+
+    public static void christmasTree(int height) {
+        // part 3 of the exercise
+        int i = 1;
+        int j = height - 1;
+        
+        while(i <= height * 2 && j >= 0){
+            printSpaces(j);
+            printStars(i);
+            i += 2;
+            j--;
+            if(j == -1){
+                printSpaces(height - 2);
+                System.out.println("***");
+                printSpaces(height - 2);
+                System.out.println("***");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // The tests are not checking the main, so you can modify it freely.
+
+        printTriangle(5);
+        System.out.println("---");
+        christmasTree(4);
+        System.out.println("---");
+        christmasTree(7);
+    }
+}
